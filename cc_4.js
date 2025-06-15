7c80b966089e9c4c31f31806e8f1c10580013495
markalfieri2 <markalfieri@usf.edu>
2025-06-14 22:43:24 -0400

    Added randomization of customer orders, as well as the ability to see how much of each item the customers ordered.

--- cc_4.js
+++ cc_4.js
@@ -36,24 +36,39 @@ for (let i = 0; i < 3; i++) {
   }
 
   let totalCost = 0;
+  // Track purchases for this customer
+  let purchases = [];
+
   for (let product of products) {
-    if (product.inventoryCount > 0) {
-      totalCost += product.price * (1 - additionalDiscount);
-      product.inventoryCount--;
+    // Random amount between 0 and 5, but not more than inventoryCount
+    let maxPurchase = Math.min(5, product.inventoryCount);
+    let purchased = Math.floor(Math.random() * (maxPurchase + 1));
+    if (purchased > 0) {
+      totalCost += product.price * (1 - additionalDiscount) * purchased;
+      product.inventoryCount -= purchased;
     }
+    purchases.push({ name: product.name, purchased });
   }
 
   console.log(`Customer ${i + 1} (${customerType}): Total cost = $${totalCost.toFixed(2)}`);
+  console.log("Items purchased:");
+  for (let item of purchases) {
+    console.log(`- ${item.name}: ${item.purchased}`);
+  }
 
-  // Log all products' info after discounts and inventory update
-  for (let product of products) {
-    // for...in loop
-    for (let key in product) {
-      console.log(`${key}: ${product[key]}`);
-    }
-    // Object.entries with destructuring
-    for (let [key, value] of Object.entries(product)) {
-      console.log(`${key}: ${value}`);
+  // Log all products: use for...in for odd-indexed, Object.entries for even-indexed
+  for (let j = 0; j < products.length; j++) {
+    let product = products[j];
+    if (j % 2 === 0) {
+      console.log(`for...in breakdown (${product.name}):`);
+      for (let key in product) {
+        console.log(`${key}: ${product[key]}`);
+      }
+    } else {
+      console.log(`Object.entries breakdown (${product.name}):`);
+      for (let [key, value] of Object.entries(product)) {
+        console.log(`${key}: ${value}`);
+      }
     }
     console.log('---');
   }
